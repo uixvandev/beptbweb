70173a993002a36e4479740430d3357b03c5abe8
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2023-06-26 21:15:12 +0700

    deploy vercel2

--- config/database.js
+++ config/database.js
@@ -2,11 +2,18 @@ const mysql = require("mysql");
 
 // untuk konfigurasi koneksi
 const koneksi = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "db_tb",
-  multipleStatements: true,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  // host: "localhost",
+  // user: "root",
+  // password: "pw_db",
+  // database: "db_tb",
+  // multipleStatements: true,
 });
 
 // koneksi database
