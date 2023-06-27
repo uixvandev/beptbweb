60223ae6c47028d1f502795c7054b06cec6a0d5a
irfan wahendra <89741043+uixvandev@users.noreply.github.com>
2023-06-27 17:23:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,14 @@ const url = require("url");
 const cors = require("cors");
 
 const multer = require("multer");
-const os = require("os");
-
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/document");
+    cb(null, "./document");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   },
 });
-
 var upload = multer({ storage: storage });
 
 const { register, login, profil, resetPassword, userSelf } = require("./modul/user");
